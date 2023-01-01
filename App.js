b36dc068dd9d0aa16643fa418521e6dcb6f7e386
divinejoshua <divineuche399@gmail.com>
2023-01-01 14:06:41 +0000

    Completed first mobile app with webview

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Divine new app</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Divine new app</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <WebView
+        source={{
+          uri: 'https://qeloop.pages.dev',
+        }}
+        style={{marginTop: 30}}
+      />
   );
 }
 
